cb8b223299002277f45024c46fbef4e04232e052
mofakkharul-29 <mofakkharulislam29.info@gmail.com>
2026-03-10 03:47:14 +0600

    elevated button is added

--- lib/features/auth/widgets/log_reg_form.dart
+++ lib/features/auth/widgets/log_reg_form.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:p_papper/core/utils/custom_elevated_button.dart';
+import 'package:p_papper/core/utils/custom_text.dart';
 import 'package:p_papper/features/auth/widgets/custom_text_form_field.dart';
 
 class LogRegForm extends StatelessWidget {
@@ -6,22 +8,39 @@ class LogRegForm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final double buttonWidth = MediaQuery.of(
+      context,
+    ).size.width;
+
     return Form(
       child: Column(
         children: [
           CustomTextFormField(
             icon: Icons.email_rounded,
             labelText: 'Email',
-          ),
-          const SizedBox(height: 10),
-          CustomTextFormField(
-            icon: Icons.person_rounded,
-            labelText: 'Name',
+            keyboardType: TextInputType.emailAddress,
           ),
           const SizedBox(height: 10),
           CustomTextFormField(
             icon: Icons.lock_rounded,
             labelText: 'Password',
+            obscureText: true,
+          ),
+          const SizedBox(height: 15),
+          CustomElevatedButton(
+            onPressed: () {},
+            bgColor: Colors.blue,
+            elevation: 1.5,
+            fixedSize: Size(buttonWidth * 0.92, 53),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadiusGeometry.circular(5),
+            ),
+            child: CustomText(
+              text: 'login',
+              color: Colors.black87,
+              fontSize: 18,
+              fontWeight: FontWeight.w800,
+            ),
           ),
         ],
       ),
